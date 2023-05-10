2abddda4a21d9480dcd39778097757bd1f69052f
nickspoon <nick@jense.nz>
2023-05-11 00:49:43 +1200

    Complete status page layout

--- scripts/status.js
+++ scripts/status.js
@@ -1,12 +1,40 @@
 st.status = (() => {
-   const $pre = dbi('pre-status')
-   const $button = dbi('stat-load')
-   $button.onclick = () => load()
-
    const load = async () => {
       const data = await st.api.get('')
-      console.log(data)
-      $pre.textContent = JSON.stringify(data, null, 2)
+      st.view.current.innerHTML = `
+<h2>Status</h2>
+<p>${data.status}</p>
+
+<dl>
+  <dt>Agents</dt>
+  <dd>${data.stats.agents}</dd>
+  <dt>Ships</dt>
+  <dd>${data.stats.ships}</dd>
+  <dt>Systems</dt>
+  <dd>${data.stats.systems}</dd>
+  <dt>Waypoints</dt>
+  <dd>${data.stats.waypoints}</dd>
+</dl>
+
+<h2>Leaderboards</h2>
+
+<div class="wrapping-row">
+  <div>
+    <h3>Most credits</h3>
+    ${totable(
+       data.leaderboards.mostCredits,
+       i => `<td>${i.agentSymbol}</td><td style="text-align: right">${i.credits.toLocaleString()}</td>`,
+       '<th>Agent</th><th style="text-align: right">Credits</th>')}
+  </div>
+  <div>
+    <h3>Most submitted charts</h3>
+    ${totable(
+       data.leaderboards.mostSubmittedCharts,
+       i => `<td>${i.agentSymbol}</td><td style="text-align: right">${i.chartCount.toLocaleString()}</td>`,
+       '<th>Agent</th><th style="text-align: right">Chart count</th>')}
+  </div>
+</div>
+`
       st.view.update()
    }
 
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,9 @@
 const dbi = id => document.getElementById(id)
 
 const dbt = (element, tag) => element.getElementsByTagName(tag)[0]
+
+const totable = (items, genrow, header) => `
+<table>${header ? `<tr>${header}</tr>` : ''}
+  ${items.map(i => `<tr>${genrow(i)}</tr>`).join('\n')}
+</table>
+`
