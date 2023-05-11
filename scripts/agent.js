fd817f9c339cb2bd1308a4703be43890d7e06108
nickspoon <nick@jense.nz>
2023-05-12 00:55:19 +1200

    Make agent display fully dynamic

--- scripts/agent.js
+++ scripts/agent.js
@@ -3,18 +3,20 @@ st.agent = (() => {
 
    const $pre = dbi('pre-agent')
    const load = async () => {
-      if (!st.state.token)
-         return
       const agentData = await st.api.get('my/agent')
       if (!agentData)
          return false
       _data = agentData.data
-      const display = {
-         handle: _data.symbol,
-         headquarters: _data.headquarters,
-         credits: _data.credits
-      }
-      $pre.textContent = JSON.stringify(display, null, 2)
+      st.view.current.innerHTML = `
+<h2>${_data.symbol}</h2>
+
+<dl>
+  <dt>Headquarters</dt>
+  <dd>${_data.headquarters}</dd>
+  <dt>Credits</dt>
+  <dd>${_data.credits}</dd>
+</dl>
+`
       return true
    }
 
