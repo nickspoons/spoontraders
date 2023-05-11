fec41b45fdf29ccb800d14a3eada97de76bb9657
nickspoon <nick@jense.nz>
2023-05-12 11:45:59 +1200

    Update waypoint styles

--- scripts/waypoint.js
+++ scripts/waypoint.js
@@ -15,32 +15,47 @@ st.waypoint = (() => {
       st.view.current.innerHTML = `
 <h2>System: ${systemID}</h2>
 
-${waypointsData.data.map((wp, i) => `
-${i !== 0 ? `<hr>` : ''}
-<dl>
-  <dt>Waypoint</dt>
-  <dd>${split(wp.symbol).waypointID} - ${wp.faction.symbol}</dd>
-  <dt>Type</dt>
-  <dd>${wp.type}</dd>
-  <dt>Coordinates</dt>
-  <dd>${wp.x}:${wp.y}</dd>
-  <dt>Orbitals</dt>
-  <dd>${wp.orbitals.map(o => `<a href="" data-id="${o.symbol}">${o.symbol}</a>`).join('')}</dd>
-  <dt>Chart submitted</dt>
-  <dd>${new Date(wp.chart.submittedOn).toLocaleDateString()}, ${wp.chart.submittedBy}</dd>
-</dl>
-${wp.traits.map(t => `<div class="trait" title="${t.description}">${t.symbol}: ${t.name}</div>`).join('')}
+<div class="seed-${seed}">
+  ${waypointsData.data.map(wp => `
+  <div class="waypoint">
+    <h3 title="${split(wp.symbol).waypointID} - ${wp.faction.symbol}">
+      ${split(wp.symbol).waypointID}
+    </h3>
+    <dl>
+      <dt>Type</dt>
+      <dd>${wp.type}</dd>
+      <dt>Coordinates</dt>
+      <dd>${wp.x}:${wp.y}</dd>
 
+      ${wp.orbitals.length
+      ? `<dt>Orbitals</dt>
+      <dd>${wp.orbitals.map(o => `<a href="" data-id="${o.symbol}">${o.symbol}</a>`).join('')}</dd>`
+      : ''}
+
+      <dt>Chart submitted</dt>
+      <dd>${new Date(wp.chart.submittedOn).toLocaleDateString()}, ${wp.chart.submittedBy}</dd>
+    </dl>
+    ${wp.traits.map(t => `<span class="trait" title="${t.symbol}: ${t.description}">${t.name}</span>`).join(', ')}
+  </div>
 `).join('')}
+</div>
 
 
 <style>
-  .seed-${seed} table {
-    max-width: 30em;
+  .seed-${seed} {
+    display: flex;
+    flex-basis: 0;
+    flex-direction: column;
+    gap: 0.5em;
   }
-  .seed-${seed} table tr *:last-child {
-    text-align: right;
+  .seed-${seed} .waypoint {
+    background-color: #1d2021;
+    padding: 0.5em;
+    border: 1px solid #ebdbb29a;;
+    border-radius: 0.5em 0;
+    position: relative;
   }
+
 </style>
 `
       // dbi(`btn-${seed}`).onclick = () => load()
