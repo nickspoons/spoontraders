a67c9b1b176e80ea7bea586251b11bd5411013da
nickspoon <nick@jense.nz>
2023-05-13 23:19:38 +1200

    Normalise registration view handling

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,4 +1,44 @@
 (() => {
+   const seed = genSeed()
+   st.view.get(st.view.View.REGISTRATION).innerHTML = `
+<div class="wrapping-row seed-${seed}">
+   <form id="reg-form">
+      <h2>You need to register</h2>
+      <label for="reg-handle">Handle</label>
+      <input placeholder="Handle" id="reg-handle" name="reg-handle" required />
+      <label for="reg-faction">Faction</label>
+      <input placeholder="Faction" id="reg-faction" name="reg-faction" value="COSMIC" required />
+      <button type="submit">
+         Register
+      </button>
+   </form>
+   <form id="ver-form">
+      <h2>... or enter an access token</h2>
+      <label for="ver-token">Access token</label>
+      <input placeholder="Access token" id="ver-token" name="ver-token" required />
+      <button type="submit">
+         Verify
+      </button>
+   </form>
+</div>
+<style>
+   .seed-${seed} form {
+      display: flex;
+      flex-direction: column;
+   }
+   .seed-${seed} label {
+      font-size: smaller;
+      margin-top: 0.5em;
+      opacity: 0.6;
+   }
+   .seed-${seed} input {
+      max-width: 20em;
+   }
+   .seed-${seed} button {
+      place-self: flex-start;
+   }
+</style>
+`
    const $regForm = dbi('reg-form')
    const $regHandle = dbi('reg-handle')
    const $regFaction = dbi('reg-faction')
