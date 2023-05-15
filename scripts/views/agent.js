2a6303ff0e6ed3901d74e1513adf0ded0e649615
nickspoon <nick@jense.nz>
2023-05-16 09:20:02 +1200

    Add log-out and access-token display functionality

--- scripts/views/agent.js
+++ scripts/views/agent.js
@@ -17,11 +17,48 @@ st.views.agent = (() => {
    <dt>Credits</dt>
    <dd>${data.credits}</dd>
 </dl>
+
+<details class="details-${seed}">
+   <summary>Access token</summary>
+   <pre>${st.state.token}</pre>
+</details>
+
+<button id="button-${seed}">
+   Log out
+</button>
+
+<style>
+   .details-${seed} summary {
+      cursor: pointer;
+      display: inline;
+      outline: none;
+   }
+   .details-${seed} summary:focus-visible {
+      outline: auto;
+   }
+   .details-${seed} pre {
+      background-color: #1d2021;
+      border: 1px solid #928374;
+      border-radius: 0.3em 0;
+      padding: 0.5em;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+   }
+</style>
 `
       dbi(`a-${seed}`).onclick = ce => {
          ce.preventDefault()
          st.view.navigate(st.view.View.WAYPOINT)
          st.views.waypoint.load(data.headquarters)
+      }
+      dbi(`button-${seed}`).onclick = () => {
+         st.cache.token = null
+         st.state.token = null
+         st.state.registered = false
+         st.agent.headquarters = null
+         st.view.navigate(st.view.View.REGISTRATION)
+
+         st.view.update()
       }
       return true
    }
