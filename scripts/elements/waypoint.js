3963330fbc45c6c7c4e186ef9eeb9bb9f88e23dc
nickspoon <nick@jense.nz>
2023-05-16 20:45:11 +1200

    Refactor waypoint rendering to use the new data

---
+++ scripts/elements/waypoint.js
@@ -0,0 +1,25 @@
+st.elements.waypoint = (() => {
+
+   const renderDetails = wp => `
+<h3 title="${wp.symbol} - ${wp.faction.symbol}"
+      style="color: ${st.colors.waypoint[wp.type] || st.colors.waypoint.unknown}">
+   ${wp.symbol}
+</h3>
+<dl title="${`
+   Coordinates ${wp.x}:${wp.y}
+   Chart submitted: ${new Date(wp.chart.submittedOn).toLocaleDateString()}, ${wp.chart.submittedBy}
+`}">
+   <dt>Type</dt>
+   <dd>${wp.type}</dd>
+</dl>
+${wp.traits.map(t => `<span class="trait" title="${t.symbol}: ${t.description}">${t.name}</span>`).join(', ')}`
+
+   const render = wp => `
+<div class="waypoint">
+   ${renderDetails(wp)}
+   ${wp.orbitals.map(o => '<hr>' + renderDetails(o)).join('')}
+</div>
+`
+
+   return { render }
+})()
